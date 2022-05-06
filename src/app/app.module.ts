d04461062438bc1f0498ecaa97bd3ba46cdbde94
Alexey Sapr <alexeysapr@gmail.com>
2022-05-06 17:07:40 +0300

    add register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RegisterComponent
-  ],
+  declarations: [AppComponent, RegisterComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  public registerForm: FormGroup;
+  public hide: boolean = true;
 
-  constructor() { }
+  public passwordRegexp: RegExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
 
-  ngOnInit(): void {
+  constructor() {
+    this.registerForm = new FormGroup({
+      firstName: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(25),
+      ]),
+      lastName: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(25),
+      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(this.passwordRegexp),
+      ]),
+    });
   }
 
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log('registerForm: ', this.registerForm);
+    console.log('registerForm: ', this.registerForm.value);
+    this.registerForm.reset();
+  }
+
+  getNameErrorMessage(inputField: string): string | undefined {
+    if (this.registerForm.hasError('required', inputField)) {
+      return 'You must enter a value';
+    }
+    if (this.registerForm.hasError('minlength', inputField)) {
+      return 'The minimum length for this field is 2 characters';
+    }
+    if (this.registerForm.hasError('email', inputField)) {
+      return 'Not a valid email';
+    }
+    if (this.registerForm.hasError('pattern', inputField)) {
+      return 'The	password must contain minimum six	characters, at least one letter and one number';
+    }
+
+    return;
+  }
 }
