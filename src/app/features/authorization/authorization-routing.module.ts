57ec70037ade1401faf772a084106c6c6334015e
Alexey Sapr <alexeysapr@gmail.com>
2022-05-07 19:14:36 +0300

    add register page markup

--- src/app/features/authorization/authorization-routing.module.ts
+++ src/app/features/authorization/authorization-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthorizationComponent } from './authorization.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 /*
 List here all routes related to authorization module.
@@ -10,14 +11,14 @@ Routes for singIn, singUp components
 const routes: Routes = [
 	{
 		path: 'authorization',
-		component: AuthorizationComponent
-		// children: [
-		// 	{ path: 'signIn', component: SignInComponent },
-		// 	{
-		// 		path: 'signUp',
-		// 		component: SingUpComponent
-		// 	}
-		// ]
+		component: AuthorizationComponent,
+		children: [
+			// { path: 'signIn', component: SignInComponent },
+			{
+				path: 'register',
+				component: RegisterComponent
+			}
+		]
 	}
 ];
 
---
+++ src/app/features/authorization/pages/register/register.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+	selector: 'app-register',
+	templateUrl: './register.component.html',
+	styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+	public registerForm: FormGroup;
+	public hide: boolean = true;
+
+	public passwordRegexp: RegExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
+
+	constructor() {
+		this.registerForm = new FormGroup({
+			firstName: new FormControl(null, [
+				Validators.required,
+				Validators.minLength(2),
+				Validators.maxLength(25)
+			]),
+			lastName: new FormControl(null, [
+				Validators.required,
+				Validators.minLength(2),
+				Validators.maxLength(25)
+			]),
+			email: new FormControl(null, [Validators.required, Validators.email]),
+			password: new FormControl(null, [
+				Validators.required,
+				Validators.pattern(this.passwordRegexp)
+			])
+		});
+	}
+
+	onSubmit() {
+		console.log('registerForm: ', this.registerForm);
+		console.log('registerForm: ', this.registerForm.value);
+		this.registerForm.reset();
+	}
+
+	getNameErrorMessage(inputField: string): string | undefined {
+		if (this.registerForm.hasError('required', inputField)) {
+			return 'You must enter a value';
+		}
+		if (this.registerForm.hasError('minlength', inputField)) {
+			return 'The minimum length for this field is 2 characters';
+		}
+		if (this.registerForm.hasError('email', inputField)) {
+			return 'Not a valid email';
+		}
+		if (this.registerForm.hasError('pattern', inputField)) {
+			return 'The	password must contain minimum six	characters, at least one letter and one number';
+		}
+
+		return;
+	}
+}
