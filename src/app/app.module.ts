27d8e16afb5fa7a45e76f9e120feec30db580086
dennissimmo <denchik16062001@gmail.com>
2022-05-08 19:07:58 +0300

    [Firebase] Install firebase, setup config, install Firestore and Authorization. Provide firebase application, auth and firestore. Fix path for environment file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './core/components/header/header.component';
-import { CoreModule } from './core/core.module';
+import { HeaderComponent } from '@core/components/header/header.component';
+import { CoreModule } from '@core/core.module';
 import { AuthorizationModule } from './features/authorization/authorization.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '@env';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
 @NgModule({
 	declarations: [AppComponent, HeaderComponent],
@@ -16,9 +21,17 @@ import { AppRoutingModule } from './app-routing.module';
 		AppRoutingModule,
 		CoreModule,
 		AuthorizationModule,
-		MatToolbarModule
+		MatToolbarModule,
+		provideFirebaseApp(() => initializeApp(environment.firebase)),
+		provideAuth(() => getAuth()),
+		provideFirestore(() => getFirestore())
 	],
-	providers: [],
+	providers: [
+		{
+			provide: FIREBASE_OPTIONS,
+			useValue: environment.firebase
+		}
+	],
 	exports: [HeaderComponent],
 	bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-	production: false
+	production: false,
+	firebase: {
+		projectId: 'wiki-project-d5644',
+		appId: '1:923657900032:web:ff70e28bf7190f393408e5',
+		storageBucket: 'wiki-project-d5644.appspot.com',
+		locationId: 'europe-central2',
+		apiKey: 'AIzaSyC01z_rWrunkcIuXIe5hbRYl09egEKzR8w',
+		authDomain: 'wiki-project-d5644.firebaseapp.com',
+		messagingSenderId: '923657900032',
+		measurementId: 'G-H6BHSF19B5'
+	}
 };
 
 /*
