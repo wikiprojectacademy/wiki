49bbe28f4d9c8ce3731e71a21086199e75835aaa
dennissimmo <denchik16062001@gmail.com>
2022-05-08 19:11:02 +0300

    Fix header import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from '@core/components/header/header.component';
 import { CoreModule } from '@core/core.module';
 import { AuthorizationModule } from './features/authorization/authorization.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -12,6 +11,7 @@ import { environment } from '@env';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { HeaderComponent } from '@core/components/header/header.component';
 
 @NgModule({
 	declarations: [AppComponent, HeaderComponent],
