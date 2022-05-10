2b4c7c44f6cb98d08bbae910590393b7bc5cf2f0
Roman Savran <roblumination@gmail.com>
2022-05-10 19:19:23 +0300

    remove spec files

--- src/app/features/main/pipes/content-shortner.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ContentShortnerPipe } from './content-shortner.pipe';
-
-describe('ContentShortnerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ContentShortnerPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
