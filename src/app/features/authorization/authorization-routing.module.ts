1cdef7e5b840e3e861b6768e6cb414f90cbcd648
Alexey Sapr <alexeysapr@gmail.com>
2022-05-11 15:40:14 +0300

    add routes guards for login and register

--- src/app/features/authorization/authorization-routing.module.ts
+++ src/app/features/authorization/authorization-routing.module.ts
@@ -2,20 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { redirectLoggedInTo } from '@angular/fire/compat/auth-guard';
+import { canActivate } from '@angular/fire/compat/auth-guard';
 
 /*
 List here all routes related to authorization module.
 Routes for singIn, singUp components
  */
+const redirectLoggedInToMain = () => redirectLoggedInTo(['main']);
 
 const routes: Routes = [
 	{
 		path: '',
 		children: [
-			{ path: 'login', component: LoginComponent },
+			{
+				path: 'login',
+				component: LoginComponent,
+				...canActivate(redirectLoggedInToMain)
+			},
 			{
 				path: 'register',
-				component: RegisterComponent
+				component: RegisterComponent,
+				...canActivate(redirectLoggedInToMain)
 			}
 		]
 	}
