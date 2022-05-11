08154849b0cbbe12a1cb0960471f9401411926a7
Roman Savran <roblumination@gmail.com>
2022-05-11 16:33:12 +0300

    add height fix

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -5,12 +5,8 @@ import { Component } from '@angular/core';
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.scss']
 })
-<<<<<<< HEAD
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 	// TO DO
 	isLoggedIn: boolean = false;
-=======
-export class HeaderComponent {
->>>>>>> 21abffb9668fe5dde2e4f74b9e32a8b7f3789a53
 	constructor() {}
 }
