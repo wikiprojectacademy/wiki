8798ebaf33d05b6ddf360d9d18440dc03d1e592f
dennissimmo <denchik16062001@gmail.com>
2022-05-13 11:59:51 +0300

    [Categories Service] Add a few methods to example of usage firebase CRUD service

--- src/app/core/services/categoryFirebase.service.ts
+++ src/app/core/services/categoryFirebase.service.ts
@@ -4,6 +4,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Injectable } from '@angular/core';
 import { ISubCategory } from '@core/models/SubCategory';
 import { Observable } from 'rxjs';
+import { subCategories } from '@core/services/initial-data';
 
 /**
  * Small example of usage Firebase CRUD service for operations with categories
