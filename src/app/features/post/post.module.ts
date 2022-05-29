28b1dedf2825e15361336157cb5f7103918c0834
Edgar <selectsegment@gmail.com>
2022-05-29 23:29:24 +0300

    Created page for displaying post

--- src/app/features/post/post.module.ts
+++ src/app/features/post/post.module.ts
@@ -5,16 +5,19 @@ import { MatOptionModule, MatRippleModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { PostRoutingModule } from './post-routing.module';
-import { PostListComponent } from './pages/post-list/post-list.component';
 import { PostAddComponent } from './pages/post-add/post-add.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { NgxEditorModule } from 'ngx-editor';
+import { PostComponent } from './pages/post/post.component';
+import { SafeHtmlPipe } from './pipes/safe-html.pipe';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
-	declarations: [PostListComponent, PostAddComponent],
+	declarations: [PostAddComponent, PostComponent, SafeHtmlPipe, SafeHtmlPipe],
 	imports: [
 		CommonModule,
 		PostRoutingModule,
@@ -28,7 +31,9 @@ import { NgxEditorModule } from 'ngx-editor';
 		MatOptionModule,
 		MatSelectModule,
 		NgxEditorModule,
-		FormsModule
+		FormsModule,
+		MatProgressSpinnerModule,
+		MatDividerModule
 	]
 })
 export class PostModule {}
--- src/app/features/post/services/post.service.ts
+++ src/app/features/post/services/post.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { PostFirebaseService } from '@core/services/firebase/firebase-entities/postFirebase.service';
 import { IPost } from '@core/models/Post';
+import { Observable } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root'
@@ -14,5 +15,9 @@ export class PostService {
 
 	addPostWithCustomId(id: string, post: IPost): Promise<void> {
 		return this.postFirebaseService.addPostWithCustomId(id, post);
+	}
+
+	getPost(id: string): Observable<IPost> | undefined {
+		return this.postFirebaseService.getPost(id);
 	}
 }
