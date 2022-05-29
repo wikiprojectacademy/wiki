2bee044d7f3c8b7a6eb6b528076ae0fa6112f14c
Alexey Sapr <alexeysapr@gmail.com>
2022-05-29 11:25:45 +0300

    add getUserByEmail method to UserFirebaseService

--- src/app/core/services/firebase/firebase-entities/userFirebase.service.ts
+++ src/app/core/services/firebase/firebase-entities/userFirebase.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { FirebaseCrudService } from '@core/services/firebase/firebase-api/firebaseCrud.service';
 import { IUser } from '@core/models/User';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { lastValueFrom, map, Observable, take } from 'rxjs';
 import { IRole } from '@core/models/Role';
 
 @Injectable({
@@ -24,11 +24,11 @@ export class UserFirebaseService extends FirebaseCrudService<
 
 	addUserWithRole(userId: string, user: IUser, role: IRole) {}
 
-	updateUser(id: string, content: IUser) {
+	updateUser(id: string, content: IUser): Promise<void> {
 		return this.updateDoc(id, content);
 	}
 
-	getUserData(id: string) {
+	getUserData(id: string): Observable<IUser> {
 		return this.getDocSnapshot(id);
 	}
 
@@ -38,5 +38,21 @@ export class UserFirebaseService extends FirebaseCrudService<
 
 	addUserWithCustomId(id: string, user: IUser): Promise<void> {
 		return this.addDoc(id, user);
+	}
+
+	getUserByEmail(email: string): Promise<IUser> {
+		const userFromDatabase$ = this.getUsers$().pipe(
+			map(data => {
+				const user = data.find(user => user.email === email);
+				if (!user) {
+					throw Error('User not found in database');
+				}
+
+				return user;
+			}),
+			take(1)
+		);
+
+		return lastValueFrom(userFromDatabase$).then((user: IUser) => user);
 	}
 }
