2d2c74ab4202581efe86cfb7c58d99e6548298fa
dennissimmo <denchik16062001@gmail.com>
2022-05-30 23:26:16 +0300

    [Current user service] Add isAdmin and isDatabaseInitialized subjectects

--- src/app/core/services/user/current-user.service.ts
+++ src/app/core/services/user/current-user.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { map, of, shareReplay, switchMap, takeLast } from 'rxjs';
+import {
+	BehaviorSubject,
+	map,
+	Observable,
+	of,
+	shareReplay,
+	switchMap,
+	tap
+} from 'rxjs';
 import { IUser } from '@core/models/User';
 import { UserFirebaseService } from '../firebase/firebase-entities/userFirebase.service';
 
@@ -8,19 +16,40 @@ import { UserFirebaseService } from '../firebase/firebase-entities/userFirebase.
 	providedIn: 'root'
 })
 export class CurrentUserService {
-	public currentUser$ = this.afAuth.user.pipe(
-		switchMap(user =>
-			user
-				? this.userFireStore.getUserData(user.uid)
-				: of({ roleId: '' } as IUser)
-		),
-		shareReplay(1)
-	);
-
-	public isUserLogin$ = this.currentUser$.pipe(map(user => user.roleId !== ''));
+	public currentUser$: Observable<IUser>;
+	public isUserLogin$: Observable<boolean>;
+	public isAdmin: BehaviorSubject<boolean>;
+	public isDatabaseInitialized: BehaviorSubject<boolean>;
 
 	constructor(
 		private afAuth: AngularFireAuth,
 		private userFireStore: UserFirebaseService
-	) {}
+	) {
+		this.isAdmin = new BehaviorSubject<boolean>(false);
+		this.isDatabaseInitialized = new BehaviorSubject<boolean>(true);
+		this.initUser();
+	}
+
+	initUser(): void {
+		this.currentUser$ = this.afAuth.user.pipe(
+			switchMap(user =>
+				user
+					? this.userFireStore.getUserData(user.uid)
+					: of({ roleId: '' } as IUser)
+			),
+			shareReplay(1)
+		);
+
+		this.isUserLogin$ = this.currentUser$.pipe(
+			tap(user => {
+				if (user?.roleId !== '' && user?.isAdmin) {
+					this.isAdmin.next(true);
+					this.isDatabaseInitialized.next(user?.isDatabaseInitialized);
+				} else {
+					this.isAdmin.next(false);
+				}
+			}),
+			map(user => user?.roleId !== '')
+		);
+	}
 }
