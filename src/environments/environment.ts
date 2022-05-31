62b9313def580ead24e21456e75e4d98b5410eb1
dennissimmo <denchik16062001@gmail.com>
2022-05-31 11:56:05 +0300

    [Deployment] Replace production firebase configuration, expand minimal available bundle size, update firebase project ids default and production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,17 +12,6 @@ export const environment = {
 	 *
 	 */
 
-	// firebase: {
-	// 	projectId: 'wiki-project-d5644',
-	// 	appId: '1:923657900032:web:ff70e28bf7190f393408e5',
-	// 	storageBucket: 'wiki-project-d5644.appspot.com',
-	// 	locationId: 'europe-central2',
-	// 	apiKey: 'AIzaSyC01z_rWrunkcIuXIe5hbRYl09egEKzR8w',
-	// 	authDomain: 'wiki-project-d5644.firebaseapp.com',
-	// 	messagingSenderId: '923657900032',
-	// 	measurementId: 'G-H6BHSF19B5'
-	// }
-
 	firebase: {
 		apiKey: 'AIzaSyAW7Nn9EFtOzzpLf5HMlqFGEfdGEkklgF4',
 		authDomain: 'wiki-3fc1f.firebaseapp.com',
@@ -31,6 +20,15 @@ export const environment = {
 		messagingSenderId: '667380241142',
 		appId: '1:667380241142:web:f9190dd857f4f322a492d7'
 	}
+
+	// firebase: {
+	// 	apiKey: 'AIzaSyAW7Nn9EFtOzzpLf5HMlqFGEfdGEkklgF4',
+	// 	authDomain: 'wiki-3fc1f.firebaseapp.com',
+	// 	projectId: 'wiki-3fc1f',
+	// 	storageBucket: 'wiki-3fc1f.appspot.com',
+	// 	messagingSenderId: '667380241142',
+	// 	appId: '1:667380241142:web:f9190dd857f4f322a492d7'
+	// }
 };
 
 /*
