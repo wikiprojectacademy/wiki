746f164b7ab1302393be235e6d2c99bfac8d5375
Alexey Sapr <alexeysapr@gmail.com>
2022-06-04 10:31:40 +0300

    refactor current user service

--- src/app/core/services/user/current-user.service.ts
+++ src/app/core/services/user/current-user.service.ts
@@ -17,30 +17,10 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 	providedIn: 'root'
 })
 export class CurrentUserService {
-	public currentUser$: Observable<IUser> = this.afAuth.user.pipe(
-		switchMap(user => {
-			console.log('new user action');
-			return user
-				? this.userFireStore
-						.getUserDataByEmail(user.email)
-						.pipe(catchError(() => of({ roleId: '' } as IUser)))
-				: of({ roleId: '' } as IUser);
-		}),
-		shareReplay(1)
-	);
-	public isUserLogin$: Observable<boolean> = this.currentUser$.pipe(
-		tap(userData => {
-			if (userData?.roleId !== '' && userData?.isAdmin) {
-				this.isAdmin.next(true);
-				this.isDatabaseInitialized.next(userData?.isDatabaseInitialized);
-			} else {
-				this.isAdmin.next(false);
-			}
-		}),
-		map(user => user?.roleId !== '')
-	);
 	public isAdmin: BehaviorSubject<boolean>;
 	public isDatabaseInitialized: BehaviorSubject<boolean>;
+	public currentUser$: Observable<IUser>;
+	public isUserLogin$: Observable<boolean>;
 
 	constructor(
 		private afAuth: AngularFireAuth,
@@ -48,5 +28,30 @@ export class CurrentUserService {
 	) {
 		this.isAdmin = new BehaviorSubject<boolean>(false);
 		this.isDatabaseInitialized = new BehaviorSubject<boolean>(true);
+
+		this.currentUser$ = this.afAuth.user.pipe(
+			switchMap(user => {
+				console.log('user: ', user?.email);
+				console.log('new user action');
+				return user
+					? this.userFireStore
+							.getUserDataByEmail(user.email)
+							.pipe(catchError(() => of({ roleId: '' } as IUser)))
+					: of({ roleId: '' } as IUser);
+			}),
+			shareReplay(1)
+		);
+
+		this.isUserLogin$ = this.currentUser$.pipe(
+			tap(userData => {
+				if (userData?.roleId !== '' && userData?.isAdmin) {
+					this.isAdmin.next(true);
+					this.isDatabaseInitialized.next(userData?.isDatabaseInitialized);
+				} else {
+					this.isAdmin.next(false);
+				}
+			}),
+			map(user => user?.roleId !== '')
+		);
 	}
 }
