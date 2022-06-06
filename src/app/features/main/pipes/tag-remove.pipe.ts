409354469e37903654a6fca675170357b3f116fb
Roman Savran <roblumination@gmail.com>
2022-06-06 11:47:13 +0300

    fix search

--- src/app/features/main/pipes/tag-remove.pipe.ts
+++ src/app/features/main/pipes/tag-remove.pipe.ts
@@ -1,17 +1,28 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { IPost as Post } from '@core/models/Post';
 
 @Pipe({
 	name: 'tagRemove'
 })
 export class TagRemovePipe implements PipeTransform {
-	transform(htmlText: string): string {
-		let result = '';
-		let braceCounter = 0;
-		htmlText.split('').forEach(letter => {
-			if (letter == '<') braceCounter++;
-			if (braceCounter <= 0) result += letter;
-			if (letter == '>') braceCounter--;
+	transform(posts: Post[]): Post[] {
+		posts.forEach(post => {
+			let result = '';
+			let braceCounter = 0;
+			post.contentHTML.split('').forEach(letter => {
+				if (letter == '<') braceCounter++;
+				if (braceCounter <= 0) result += letter;
+				if (letter == '>') braceCounter--;
+			});
+			post.contentHTML = result;
 		});
-		return result;
+		// let result = '';
+		// let braceCounter = 0;
+		// htmlText.split('').forEach(letter => {
+		// 	if (letter == '<') braceCounter++;
+		// 	if (braceCounter <= 0) result += letter;
+		// 	if (letter == '>') braceCounter--;
+		// });
+		return posts;
 	}
 }
