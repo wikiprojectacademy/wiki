1b39bf3f1fc4223307f3206b27a8e603d48b5f95
dennissimmo <59365385+dennissimmo@users.noreply.github.com>
2022-06-07 17:09:46 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,20 +1,12 @@
 export const environment = {
 	firebase: {
-		// apiKey: 'AIzaSyA0_LZhVlp_q3YhABHR4Oi78xcKJmGwFDA',
-		// authDomain: 'wiki-production-b014c.firebaseapp.com',
-		// projectId: 'wiki-production-b014c',
-		// storageBucket: 'wiki-production-b014c.appspot.com',
-		// messagingSenderId: '173858556181',
-		// appId: '1:173858556181:web:53f610c3d56519529a011c',
-		// measurementId: 'G-P8CE3D3MHT'
-
-		apiKey: 'AIzaSyAbhG5iZP2s2Ig1smIQiYLs0n2DLHs_V2s',
-		authDomain: 'wiki-roman.firebaseapp.com',
-		projectId: 'wiki-roman',
-		storageBucket: 'wiki-roman.appspot.com',
-		messagingSenderId: '36887117535',
-		appId: '1:36887117535:web:34a158b004c08b2f2af36a',
-		measurementId: 'G-216TM967GB'
+		apiKey: 'AIzaSyA0_LZhVlp_q3YhABHR4Oi78xcKJmGwFDA',
+		authDomain: 'wiki-production-b014c.firebaseapp.com',
+		projectId: 'wiki-production-b014c',
+		storageBucket: 'wiki-production-b014c.appspot.com',
+		messagingSenderId: '173858556181',
+		appId: '1:173858556181:web:53f610c3d56519529a011c',
+		measurementId: 'G-P8CE3D3MHT'
 	},
 	production: true
 };
